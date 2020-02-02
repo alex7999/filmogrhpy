22b64017afd7f1adb9a1606db7b1a116eded6195
Alex7999 <o.petriakov@andersenlab.com>
2020-02-02 21:11:08 +0200

    first commit

---
+++ src/main/java/testgroup/filmography/controller/Main.java
@@ -0,0 +1,23 @@
+package testgroup.filmography.controller;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/test?serverTimezone=UTC";
+        String username = "root";
+        String password = "12345678";
+        System.out.println("Connecting...");
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Connection successful!");
+        } catch (SQLException e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/testgroup/filmography/service/FilmServiceImpl.java
@@ -0,0 +1,47 @@
+package testgroup.filmography.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import testgroup.filmography.dao.FilmDAO;
+import testgroup.filmography.dao.FilmDAOImpl;
+import testgroup.filmography.model.Film;
+
+import java.util.List;
+
+@Service
+public class FilmServiceImpl implements FilmService {
+    private FilmDAO filmDAO;
+
+    @Autowired
+    public void setFilmService(FilmDAO filmDAO) {
+        this.filmDAO = filmDAO;
+    }
+
+    @Override
+    @Transactional
+    public List<Film> allFilms() {
+        return filmDAO.allFilms();
+    }
+
+    @Override
+    public void add(Film film) {
+        filmDAO.add(film);
+    }
+
+    @Override
+    public void delete(Film film) {
+        filmDAO.delete(film);
+
+    }
+
+    @Override
+    public void edit(Film film) {
+        filmDAO.edit(film);
+    }
+
+    @Override
+    public Film getById(int id) {
+        return filmDAO.getById(id);
+    }
+}
